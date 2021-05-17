107d1189ada59d156a18dd7aca4ad9db6e9f6d64
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-05-17 22:38:21 +0200

    Salam be hame !

---
+++ src/test/java/AvalinBarnameTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static java.lang.System.out;
+
+public class AvalinBarnameTest {
+
+    @Test
+    void SalambeHame() {
+        out.println("Salam be Hame shoma");
+    }
+
+    @Test
+    void HelloWorld() {
+        out.println("Hello World !");
+    }
+}
