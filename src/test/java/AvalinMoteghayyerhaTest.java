9927eb2296943214a3eed5714a61dc52dee05696
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-05-27 23:32:11 +0200

    comment & variable

---
+++ src/test/java/AvalinMoteghayyerhaTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import static java.lang.System.out;
+
+public class AvalinMoteghayyerhaTest {
+
+    @Test
+    void moteghayer_va_comment() {
+
+        // Type primitive : horof kochak
+        // Type non-primitive : horof bozorg
+
+        // Sakhtar yek moteghayer
+        // Type nam = meghdar;
+
+        // avalin moteghayer adadi
+        int adad_mosbat = 6;
+        out.println(adad_mosbat);
+        int adad_manfi = -4;
+        out.println(adad_manfi);
+        int sefr = 0;
+        out.println(sefr);
+
+        long adad_kheyli_bozorg = 20000000000L;
+        out.println(adad_kheyli_bozorg);
+
+        // moteghayer ashari
+        float adad_ashari = 1.2F;
+        out.println(adad_ashari);
+        double adad_ashari_dovom = -2.9696969;
+        out.println(adad_ashari_dovom);
+
+        // moteghayer boolean
+        boolean dorost = true;
+        boolean ghalat = false;
+        out.println(dorost);
+        out.println(ghalat);
+
+        // moteghayer horofi
+        char harf = 'c';
+        out.println(harf);
+        String kalame = "Java";
+        out.println(kalame);
+        String jomle = "I Love Java";
+        out.println(jomle);
+    }
+}
