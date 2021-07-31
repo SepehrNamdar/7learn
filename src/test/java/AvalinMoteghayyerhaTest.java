44d6cecea9286da1c21b21040a6a22a118646985
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-07-31 18:57:05 +0200

    Non primitive types

--- src/test/java/AvalinMoteghayyerhaTest.java
+++ src/test/java/AvalinMoteghayyerhaTest.java
@@ -14,6 +14,9 @@ public class AvalinMoteghayyerhaTest {
         // Type nam = meghdar;
 
         // avalin moteghayer adadi
+        Integer nonPrimitiveInteger = null;
+        nonPrimitiveInteger.toString();
+        out.println(nonPrimitiveInteger);
         int adad_mosbat = 6;
         out.println(adad_mosbat);
         int adad_manfi = -4;
@@ -22,22 +25,27 @@ public class AvalinMoteghayyerhaTest {
         out.println(sefr);
 
         long adad_kheyli_bozorg = 20000000000L;
+        Long nonPrimitiveLong;
         out.println(adad_kheyli_bozorg);
 
         // moteghayer ashari
         float adad_ashari = 1.2F;
+        Float nonPrimitiveFloat;
         out.println(adad_ashari);
         double adad_ashari_dovom = -2.9696969;
+        Double nonPrimitiveDouble;
         out.println(adad_ashari_dovom);
 
         // moteghayer boolean
         boolean dorost = true;
         boolean ghalat = false;
+        Boolean nonPrimitiveBoolean;
         out.println(dorost);
         out.println(ghalat);
 
         // moteghayer horofi
         char harf = 'c';
+        Character nonPrimitiveChracter;
         out.println(harf);
         String kalame = "Java";
         out.println(kalame);
