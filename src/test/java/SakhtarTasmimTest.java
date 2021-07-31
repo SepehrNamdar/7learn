1d93518b158538a90f6c3e95bb745268e9276dfc
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-07-31 13:14:15 +0200

    tavabe 1 : sakht tabe bedone khoroji

--- src/test/java/SakhtarTasmimTest.java
+++ src/test/java/SakhtarTasmimTest.java
@@ -3,21 +3,35 @@ import org.junit.jupiter.api.Test;
 import static java.lang.System.out;
 
 public class SakhtarTasmimTest {
+
     @Test
     void if_else() {
-        // if ( shart : boolean ) {
-        //      barame i ke bayad ejra shavad agar shart ma dorost boud (true)
-        // }
+
+        salamKon();
 
         int adad_zoj = 8;
         boolean shart = adad_zoj % 2 == 0;
         out.println(shart);
         if (shart) {
-            out.println("in adad zoj ast");
-            // ...
+            benevis("zoj");
         } else {
-            out.println("in adad fard ast");
-            // ...
+            benevis("fard");
         }
     }
+
+    private void benevis(String meghdar) {
+        out.println("in adad " + meghdar + " ast");
+    }
+
+    // Sakhtar tabe ya method
+    // mahdodeye_did type_khoroji nam_method ( parametr_1, parametr_2, ... ) {
+    //  code
+    //  return meghdar_khoroji agar type_khoroji void nabashad
+    //}
+    void salamKon() {
+        out.println("Salam");
+    }
+
+    // DRY : Don't Repeat Yourself => ba sakhtane method az in asl peyravi mikonim
+
 }
